864ab717bbfc9229bbbd46f32187a7738ff5f450
DCout <dcouto1197@gmail.com>
2019-04-11 16:27:35 +0200

    Versión 1.9.2 Quedan pendientes: -Depurado de app -Toque estético -Falta en sesion confirmada y main -Actualizar imagen de logo y nombre de proyecto.

--- app/src/main/java/com/miracosta/SesionConfirmada.java
+++ app/src/main/java/com/miracosta/SesionConfirmada.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,6 +19,7 @@ import java.util.Map;
 public class SesionConfirmada extends AppCompatActivity {
     private static final String TAG = "Miracosta";
     Button btn_cerrar, btn_refresh;
+    ImageView logo;
     FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
